fefa471971b326860c4b99725af6225fd2e5e9d3
Andre Cortes <andre.crts@gmail.com>
2017-12-15 16:56:39 -0600

    test

--- src/App.js
+++ src/App.js
@@ -4,16 +4,33 @@ import MessengerPlugin from 'react-messenger-checkbox-plugin';
 import './App.css';
 
 class App extends Component {
+
+    onClick = () => {
+        console.log(this.name);
+         this.checkBox.send('asdfasf_asdfa');
+  }
   render() {
     return (
+        <div>
+            <label>
+          COdigo de reservacion:
+          <input type="text" value={this.pnr} onChange={this.handleChange} />
+      </label>
+      <label>
+          Apellido:
+          <input type="text" value={this.name} onChange={this.handleChange} />
+        </label>
         <MessengerPlugin
             type="fb-messenger-checkbox"
-      appId="1161293877241041"
-      pageId="1192073607549305"
-      passthroughParams="WQWQQEW"
-      prechecked="true"
-      allow_login="true"
-        />
+          appId="1161293877241041"
+          pageId="362430908370"
+          prechecked="true"
+          allow_login="true"
+          onRef={ref => (this.checkBox = ref)}
+  />
+    <button onClick={this.onClick}>MessengerPlugin.send()</button>
+
+  </div>
     );
   }
 }
